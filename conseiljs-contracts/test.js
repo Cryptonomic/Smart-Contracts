c4421f9f6ccad33de5c5ce659110b5af3e3a72ae
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-07-04 18:48:18 -0400

    Add test for Tezos Proxy Redirect interface

--- conseiljs-contracts/test.js
+++ conseiljs-contracts/test.js
@@ -1,8 +1,10 @@
 "use strict";
 exports.__esModule = true;
-var TezosBakerRegistry = require("./tezos-baker-registry/TezosBakerRegistry");
-//import * as TezosProxyRedirect from './tezos-proxy-redirect/TezosProxyContract';
-//TezosBakerRegistry.deleteRegistration();
+var TezosBakerRegistry = require("./TezosBakerRegistry");
+var TezosProxyRedirect = require("./TezosProxyContract");
+// Run each line individually because the previous transaction needs to complete before the next one can activate.
 TezosBakerRegistry.updateName('"Juniper"');
 TezosBakerRegistry.updatePaymentAddress('"tz1ZfRNceHjHNqbRS5sWBNnUbct3f96fzTkU"');
 TezosBakerRegistry.updateTerms(264, 64, 16);
+TezosBakerRegistry.deleteRegistration();
+TezosProxyRedirect.setDestination('"KT1GkF5ty6vfjLhqYdaPQCcYBBDQKbUiC8vF"');
--- conseiljs-contracts/test.ts
+++ conseiljs-contracts/test.ts
@@ -1,8 +1,10 @@
-import * as TezosBakerRegistry from './tezos-baker-registry/TezosBakerRegistry';
-//import * as TezosProxyRedirect from './tezos-proxy-redirect/TezosProxyContract';
+import * as TezosBakerRegistry from './TezosBakerRegistry';
+import * as TezosProxyRedirect from './TezosProxyContract';
 
 // Run each line individually because the previous transaction needs to complete before the next one can activate.
 TezosBakerRegistry.updateName('"Juniper"');
 TezosBakerRegistry.updatePaymentAddress('"tz1ZfRNceHjHNqbRS5sWBNnUbct3f96fzTkU"');
 TezosBakerRegistry.updateTerms(264, 64, 16);
 TezosBakerRegistry.deleteRegistration();
+
+TezosProxyRedirect.setDestination('"KT1GkF5ty6vfjLhqYdaPQCcYBBDQKbUiC8vF"');
