297dec9ed3234b90159a75494807d7424297269e
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-07-13 01:47:17 -0400

    Create example smart contract invocation program for tutorial-2

---
+++ tutorials/tutorial-2/invoke-tutorial-contract-2.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var TutorialContract2 = __importStar(require("./TutorialContract2"));
+// Invoke the addIntegers entry point on an instance of Tutorial Contract 2
+TutorialContract2.addIntegers(4, 6);
---
+++ tutorials/tutorial-2/invoke-tutorial-contract-2.ts
@@ -0,0 +1,4 @@
+import * as TutorialContract2 from './TutorialContract2';
+
+// Invoke the addIntegers entry point on an instance of Tutorial Contract 2
+TutorialContract2.addIntegers(4, 6);
