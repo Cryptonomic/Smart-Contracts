65fc5d5ac938e811f00cb8a4fb0ea3db86e24b5d
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-07-23 15:35:04 -0400

    Combine deployment/invocation functionality and add documentation for TezosNameServiceCentralRegistry

--- tezos/tezos-name-service-central-registry/TezosNameServiceCentralRegistry.js
+++ tezos/tezos-name-service-central-registry/TezosNameServiceCentralRegistry.js
@@ -34,97 +34,140 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var conseiljs_1 = require("conseiljs");
-var tezosNode = 'https://tezos-dev.cryptonomic-infra.tech/';
-var keystore = {
-    publicKey: 'edpkuuGJ4ssH3N5k7ovwkBe16p8rVX1XLENiZ4FAayrcwUf9sCKXnG',
-    privateKey: 'edskRpVqFG2FHo11aB9pzbnHBiPBWhNWdwtNyQSfEEhDf5jhFbAtNS41vg9as7LSYZv6rEbtJTwyyEg9cNDdcAkSr9Z7hfvquB',
-    publicKeyHash: 'tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z',
-    seed: '',
-    storeType: conseiljs_1.StoreType.Fundraiser
-};
-var contractAddress = 'KT1GkF5ty6vfjLhqYdaPQCcYBBDQKbUiC8vF'; // Tezos Name Service - Alphanet
-function registerDomain(destination) {
+var fs_1 = __importDefault(require("fs"));
+/**
+ * Deploys an instance of the Tezos Name Service Central Registry.
+ *
+ * @param {string} initialStorage - The initial storage in Michelson
+ * @param {string} tezosNode - The URL of the Tezos node to connect to
+ * @param {KeyStore} keyStore - The sender's key store with key pair and public key hash
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+function deployContract(initialStorage, tezosNode, keyStore) {
+    if (initialStorage === void 0) { initialStorage = 'Pair {} "Author: Teckhua Chiang, Company: Cryptonomic Inc."'; }
     return __awaiter(this, void 0, void 0, function () {
-        var parameter, result;
+        var contractCode;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    parameter = destination;
-                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, conseiljs_1.TezosParameterFormat.Michelson)];
-                case 1:
-                    result = _a.sent();
-                    console.log("Injected operation group id " + result.operationGroupID);
-                    return [2 /*return*/];
+                    contractCode = fs_1.default.readFileSync('tezos-name-service-central-registry.tz', 'utf8');
+                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractOriginationOperation(tezosNode, keyStore, 0, undefined, false, true, 100000, '', 1000, 100000, contractCode, initialStorage, conseiljs_1.TezosParameterFormat.Michelson)];
+                case 1: return [2 /*return*/, _a.sent()];
+            }
+        });
+    });
+}
+exports.deployContract = deployContract;
+/**
+ * Entry point for a permitted user to register a new domain.
+ *
+ * @param {string} domain - A string representing the domain name
+ * @param {string} resolver - An address representing the resolver address
+ * @param {number} ttlInSeconds - An int representing the time to live in seconds
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+function registerDomain(domain, resolver, ttlInSeconds, invokeArgs) {
+    return __awaiter(this, void 0, void 0, function () {
+        var parameters;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    parameters = 'Left (Pair ' + domain + ' (Pair ' + resolver + ' ' + ttlInSeconds + '))';
+                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, conseiljs_1.TezosParameterFormat.Michelson)];
+                case 1: return [2 /*return*/, _a.sent()];
             }
         });
     });
 }
 exports.registerDomain = registerDomain;
-function updateResolver(destination) {
+/**
+ * Entry point for a domain owner to update the resolver for a domain.
+ *
+ * @param {string} domain - A string representing the domain name
+ * @param {string} resolver - An address representing the updated resolver address
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+function updateResolver(domain, resolver, invokeArgs) {
     return __awaiter(this, void 0, void 0, function () {
-        var parameter, result;
+        var parameters;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    parameter = destination;
-                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, conseiljs_1.TezosParameterFormat.Michelson)];
-                case 1:
-                    result = _a.sent();
-                    console.log("Injected operation group id " + result.operationGroupID);
-                    return [2 /*return*/];
+                    parameters = 'Right (Left (Pair ' + domain + ' ' + resolver + '))';
+                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, conseiljs_1.TezosParameterFormat.Michelson)];
+                case 1: return [2 /*return*/, _a.sent()];
             }
         });
     });
 }
 exports.updateResolver = updateResolver;
-function updateTTL(destination) {
+/**
+ * Entry point for a domain owner to update the time to live for a domain.
+ *
+ * @param {string} domain - A string representing the domain name
+ * @param {number} ttlInSeconds - An int representing the updated time to live in seconds
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+function updateTTL(domain, ttlInSeconds, invokeArgs) {
     return __awaiter(this, void 0, void 0, function () {
-        var parameter, result;
+        var parameters;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    parameter = destination;
-                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, conseiljs_1.TezosParameterFormat.Michelson)];
-                case 1:
-                    result = _a.sent();
-                    console.log("Injected operation group id " + result.operationGroupID);
-                    return [2 /*return*/];
+                    parameters = 'Right (Right (Left (Pair ' + domain + ' ' + ttlInSeconds + ')))';
+                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, conseiljs_1.TezosParameterFormat.Michelson)];
+                case 1: return [2 /*return*/, _a.sent()];
             }
         });
     });
 }
 exports.updateTTL = updateTTL;
-function transferOwnership(destination) {
+/**
+ * Entry point for a permitted user to transfer ownership of a domain to another user.
+ *
+ * @param {string} domain - A string representing the domain name
+ * @param {string} newOwner - A string representing the new owner address
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+function transferOwnership(domain, newOwner, invokeArgs) {
     return __awaiter(this, void 0, void 0, function () {
-        var parameter, result;
+        var parameters;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    parameter = destination;
-                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, conseiljs_1.TezosParameterFormat.Michelson)];
-                case 1:
-                    result = _a.sent();
-                    console.log("Injected operation group id " + result.operationGroupID);
-                    return [2 /*return*/];
+                    parameters = 'Right (Right (Right (Left (Pair ' + domain + ' ' + newOwner + '))))';
+                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, conseiljs_1.TezosParameterFormat.Michelson)];
+                case 1: return [2 /*return*/, _a.sent()];
             }
         });
     });
 }
 exports.transferOwnership = transferOwnership;
-function deleteDomain(destination) {
+/**
+ * Entry point for a permitted user to delete an existing domain.
+ *
+ * @param {string} domain - A string representing the domain name to be deleted
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+function deleteDomain(domain, invokeArgs) {
     return __awaiter(this, void 0, void 0, function () {
-        var parameter, result;
+        var parameters;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    parameter = destination;
-                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, conseiljs_1.TezosParameterFormat.Michelson)];
-                case 1:
-                    result = _a.sent();
-                    console.log("Injected operation group id " + result.operationGroupID);
-                    return [2 /*return*/];
+                    parameters = 'Right (Right (Right (Right (Pair ' + domain + '))))';
+                    return [4 /*yield*/, conseiljs_1.TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, conseiljs_1.TezosParameterFormat.Michelson)];
+                case 1: return [2 /*return*/, _a.sent()];
             }
         });
     });
--- tezos/tezos-name-service-central-registry/TezosNameServiceCentralRegistry.ts
+++ tezos/tezos-name-service-central-registry/TezosNameServiceCentralRegistry.ts
@@ -1,43 +1,81 @@
-import { TezosNodeWriter, StoreType, TezosParameterFormat } from 'conseiljs';
+import { TezosNodeWriter, TezosParameterFormat, OperationResult, KeyStore } from 'conseiljs';
+import fs from 'fs';
+import { InvocationArguments } from '../utilities/InvocationArguments';
 
-const tezosNode = 'https://tezos-dev.cryptonomic-infra.tech/';
-
-const keystore = {
-    publicKey: 'edpkuuGJ4ssH3N5k7ovwkBe16p8rVX1XLENiZ4FAayrcwUf9sCKXnG',
-    privateKey: 'edskRpVqFG2FHo11aB9pzbnHBiPBWhNWdwtNyQSfEEhDf5jhFbAtNS41vg9as7LSYZv6rEbtJTwyyEg9cNDdcAkSr9Z7hfvquB',
-    publicKeyHash: 'tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z',
-    seed: '',
-    storeType: StoreType.Fundraiser
-};
-
-const contractAddress = 'KT1GkF5ty6vfjLhqYdaPQCcYBBDQKbUiC8vF'; // Tezos Name Service - Alphanet
-
-export async function registerDomain(destination: string) {
-    const parameter = destination;
-    const result = await TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, TezosParameterFormat.Michelson);
-    console.log(`Injected operation group id ${result.operationGroupID}`);
+/**
+ * Deploys an instance of the Tezos Name Service Central Registry.
+ * 
+ * @param {string} initialStorage - The initial storage in Michelson
+ * @param {string} tezosNode - The URL of the Tezos node to connect to
+ * @param {KeyStore} keyStore - The sender's key store with key pair and public key hash
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+export async function deployContract(initialStorage: string = 'Pair {} "Author: Teckhua Chiang, Company: Cryptonomic Inc."', tezosNode: string, keyStore: KeyStore): Promise<OperationResult> {
+    const contractCode: string = fs.readFileSync('tezos-name-service-central-registry.tz','utf8');
+    return await TezosNodeWriter.sendContractOriginationOperation(tezosNode, keyStore, 0, undefined, false, true, 100000, '', 1000, 100000, contractCode, initialStorage, TezosParameterFormat.Michelson);
 }
 
-export async function updateResolver(destination: string) {
-    const parameter = destination;
-    const result = await TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, TezosParameterFormat.Michelson);
-    console.log(`Injected operation group id ${result.operationGroupID}`);
+/**
+ * Entry point for a permitted user to register a new domain.
+ * 
+ * @param {string} domain - A string representing the domain name
+ * @param {string} resolver - An address representing the resolver address
+ * @param {number} ttlInSeconds - An int representing the time to live in seconds
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+export async function registerDomain(domain: string, resolver: string, ttlInSeconds: number, invokeArgs: InvocationArguments): Promise<OperationResult> {
+    const parameters: string = 'Left (Pair ' + domain + ' (Pair ' + resolver + ' ' + ttlInSeconds + '))';
+    return await TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, TezosParameterFormat.Michelson);
 }
 
-export async function updateTTL(destination: string) {
-    const parameter = destination;
-    const result = await TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, TezosParameterFormat.Michelson);
-    console.log(`Injected operation group id ${result.operationGroupID}`);
+/**
+ * Entry point for a domain owner to update the resolver for a domain.
+ * 
+ * @param {string} domain - A string representing the domain name
+ * @param {string} resolver - An address representing the updated resolver address
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+export async function updateResolver(domain: string, resolver: string, invokeArgs: InvocationArguments): Promise<OperationResult> {
+    const parameters: string = 'Right (Left (Pair ' + domain + ' ' + resolver + '))';
+    return await TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, TezosParameterFormat.Michelson);
 }
 
-export async function transferOwnership(destination: string) {
-    const parameter = destination;
-    const result = await TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, TezosParameterFormat.Michelson);
-    console.log(`Injected operation group id ${result.operationGroupID}`);
+/**
+ * Entry point for a domain owner to update the time to live for a domain.
+ * 
+ * @param {string} domain - A string representing the domain name
+ * @param {number} ttlInSeconds - An int representing the updated time to live in seconds
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+export async function updateTTL(domain: string, ttlInSeconds: number, invokeArgs: InvocationArguments): Promise<OperationResult> {
+    const parameters: string = 'Right (Right (Left (Pair ' + domain + ' ' + ttlInSeconds + ')))';
+    return await TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, TezosParameterFormat.Michelson);
 }
 
-export async function deleteDomain(destination: string) {
-    const parameter = destination;
-    const result = await TezosNodeWriter.sendContractInvocationOperation(tezosNode, keystore, contractAddress, 0, 50000, '', 1000, 100000, parameter, TezosParameterFormat.Michelson);
-    console.log(`Injected operation group id ${result.operationGroupID}`);
+/**
+ * Entry point for a permitted user to transfer ownership of a domain to another user.
+ * 
+ * @param {string} domain - A string representing the domain name
+ * @param {string} newOwner - A string representing the new owner address
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+export async function transferOwnership(domain: string, newOwner: string, invokeArgs: InvocationArguments): Promise<OperationResult> {
+    const parameters: string = 'Right (Right (Right (Left (Pair ' + domain + ' ' + newOwner + '))))';
+    return await TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, TezosParameterFormat.Michelson);
+}
+
+/**
+ * Entry point for a permitted user to delete an existing domain.
+ * 
+ * @param {string} domain - A string representing the domain name to be deleted
+ * @param {InvocationArguments} invokeArgs - The arguments for a contract invocation operation
+ * @returns {Promise<OperationResult>} The result of the operation
+ */
+export async function deleteDomain(domain: string, invokeArgs: InvocationArguments): Promise<OperationResult> {
+    const parameters: string = 'Right (Right (Right (Right (Pair ' + domain + '))))';
+    return await TezosNodeWriter.sendContractInvocationOperation(invokeArgs.tezosNode, invokeArgs.keyStore, invokeArgs.contractAddress, 0, 50000, '', 1000, 100000, parameters, TezosParameterFormat.Michelson);
 }
