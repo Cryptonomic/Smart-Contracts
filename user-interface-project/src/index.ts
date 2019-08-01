bb783a4a05bc43e9bcdfb2580bb6dfdd105d3954
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-08-01 12:25:52 -0400

    Add initial modifications to generate entry points

--- user-interface-project/src/index.ts
+++ user-interface-project/src/index.ts
@@ -1,69 +1,25 @@
-import { operationArguments, operationResult } from './utilities/OperationInformation';
-import { TezosContractIntrospector, StoreType, KeyStore } from 'conseiljs';
+import { TezosContractIntrospector, KeyStore, StoreType, ConseilServerInfo, EntryPoint } from 'conseiljs';
 
-const tezosNode: string = 'https://tezos-dev.cryptonomic-infra.tech/';
+export async function generateEntryPoints() {
+    const tezosNode: string = 'https://tezos-dev.cryptonomic-infra.tech/';
+    const keystore: KeyStore = {
+        publicKey: 'edpkuuGJ4ssH3N5k7ovwkBe16p8rVX1XLENiZ4FAayrcwUf9sCKXnG',
+        privateKey: 'edskRpVqFG2FHo11aB9pzbnHBiPBWhNWdwtNyQSfEEhDf5jhFbAtNS41vg9as7LSYZv6rEbtJTwyyEg9cNDdcAkSr9Z7hfvquB',
+        publicKeyHash: 'tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z',
+        seed: '',
+        storeType: StoreType.Fundraiser
+    };
+    const network: string = 'alphanet';
+    const conseilServer: ConseilServerInfo = { url: 'https://conseil-dev.cryptonomic-infra.tech:443', apiKey: 'BUIDLonTezos-001' };
 
-const keystore: KeyStore = {
-    publicKey: 'edpkuuGJ4ssH3N5k7ovwkBe16p8rVX1XLENiZ4FAayrcwUf9sCKXnG',
-    privateKey: 'edskRpVqFG2FHo11aB9pzbnHBiPBWhNWdwtNyQSfEEhDf5jhFbAtNS41vg9as7LSYZv6rEbtJTwyyEg9cNDdcAkSr9Z7hfvquB',
-    publicKeyHash: 'tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z',
-    seed: '',
-    storeType: StoreType.Fundraiser
-};
-
-const network: string = 'alphanet';
-const conseilServer = { url: 'https://conseil-dev.cryptonomic-infra.tech:443', apiKey: 'BUIDLonTezos-001' };
-
-// Deploy Function
-
-export async function deploy() {
-    const result: operationResult = await DeployTezosManagedLedger.deployContract(tezosNode, keystore);
-    document.getElementById('console').innerHTML = `Injected operation group id ${result.operationGroupID} <br> Successfully deployed Tezos Managed Ledger`;
-}
-
-// Operation Functions
-
-export async function transfer() {
-    const from: string = (<HTMLInputElement>document.getElementById('transferFrom')).value;
-    const to: string = (<HTMLInputElement>document.getElementById('transferTo')).value;
-    const value: number = parseInt((<HTMLInputElement>document.getElementById('transferValue')).value);
-
-    const result: operationResult = await TezosManagedLedger.transfer(from, to, value, generateOpArgs());
-    renewStorage(result);
-}
-
-export async function approve() {
-    const spender: string = (<HTMLInputElement>document.getElementById('approveSpender')).value;
-    const value: number = parseInt((<HTMLInputElement>document.getElementById('approveValue')).value);
-
-    const result: operationResult = await TezosManagedLedger.approve(spender, value, generateOpArgs());
-    renewStorage(result);
+    const contractAddress: string = (<HTMLInputElement>document.getElementById('contractAddress')).value;
+    const entryPoints: EntryPoint[] = await TezosContractIntrospector.generateEntryPointsFromAddress(conseilServer, network, contractAddress);
+    document.getElementById('entryPoints').innerHTML = entryPoints.toString();
 }
 
 // Management Functions
 
-export async function setPause() {
-    const pause: boolean = (<HTMLInputElement>document.getElementById('pause')).value.toLowerCase() === "true";
-
-    let result: operationResult = await TezosManagedLedger.setPause(pause, generateOpArgs());
-    renewStorage(result);
-}
-
-export async function setAdministrator() {
-    const administrator: string = (<HTMLInputElement>document.getElementById('administrator')).value;
-
-    const result: operationResult = await TezosManagedLedger.setAdministrator(administrator, generateOpArgs());
-    renewStorage(result);
-}
-
-export async function mint() {
-    const to: string = (<HTMLInputElement>document.getElementById('mintTo')).value;
-    const value: number = parseInt((<HTMLInputElement>document.getElementById('mintValue')).value);
-
-    const result: operationResult = await TezosManagedLedger.mint(to, value, generateOpArgs());
-    renewStorage(result);
-}
-
+/*
 export async function burn() {
     const from: string = (<HTMLInputElement>document.getElementById('burnFrom')).value;
     const value: number = parseInt((<HTMLInputElement>document.getElementById('burnValue')).value);
@@ -94,27 +50,6 @@ export async function viewAllowance() {
     const address: string = '"' + (<HTMLInputElement>document.getElementById('viewAllowanceAddress')).value + '"';
     const allowance = await TezosManagedLedger.viewAllowance(address, getContractAddress(), conseilServer, network);
     document.getElementById('console').innerHTML = `Allowance from ${address} is ${allowance} <img src="./images/token.png" class="tokenIcon">`;
-}
-
-export async function viewBalance() {
-    const address: string = '"' + (<HTMLInputElement>document.getElementById('viewBalanceAddress')).value + '"';
-    const balance = await TezosManagedLedger.viewBalance(address, getContractAddress(), conseilServer, network);
-    document.getElementById('console').innerHTML = `Balance of ${address} is ${balance} <img src="./images/token.png" class="tokenIcon">`;
-}
-
-export async function viewTotalSupply() {
-    const totalSupply = await TezosManagedLedger.viewTotalSupply(getContractAddress(), conseilServer, network);
-    document.getElementById('console').innerHTML = `Total Supply: ${totalSupply} <img src="./images/token.png" class="tokenIcon">`;
-}
-
-export async function viewPaused() {
-    const isPaused = await TezosManagedLedger.viewPaused(getContractAddress(), conseilServer, network);
-    document.getElementById('console').innerHTML = `Paused: ${isPaused}`;
-}
-
-export async function viewAdministrator() {
-    const admin = await TezosManagedLedger.viewAdministrator(getContractAddress(), conseilServer, network);
-    document.getElementById('console').innerHTML = `Administrator: ${admin}`;
 }
 
 // Webpage Functions
@@ -157,3 +92,5 @@ export function generateOpArgs(): operationArguments {
         to: contractAddress
     };
 }
+
+*/
