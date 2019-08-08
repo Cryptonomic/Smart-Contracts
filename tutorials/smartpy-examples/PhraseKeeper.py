ca3c0334b00585c1b04b600235e8c1249e7a56fb
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-08-08 13:44:24 -0400

    Create PhraseKeeper SmartPy contract

---
+++ tutorials/smartpy-examples/PhraseKeeper.py
@@ -0,0 +1,17 @@
+import smartpy as sp
+class PhraseKeeper(sp.Contract):
+    def __init__(self, initialPhrase):
+        self.init(phrase = initialPhrase)
+
+    @sp.entryPoint
+    def setPhrase(self, params):
+        self.data.phrase = params.newPhrase
+
+# Tests
+@addTest(name = "Basic Test")
+def test():
+    # Instantiate the PhraseKeeper contract
+    c1 = PhraseKeeper("Hello World")
+    # Show its representation
+    html  = c1.fullHtml()
+    setOutput(html)
