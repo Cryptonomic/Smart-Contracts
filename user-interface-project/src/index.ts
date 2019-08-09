64d7a2839ddf8451e937545a2d5a205f0f4be9d2
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-08-09 16:34:25 -0400

    Add entry point invocation functionality

--- user-interface-project/src/index.ts
+++ user-interface-project/src/index.ts
@@ -1,7 +1,8 @@
-import { TezosContractIntrospector, KeyStore, StoreType, ConseilServerInfo, EntryPoint } from 'conseiljs';
+import { TezosContractIntrospector, TezosNodeWriter, ConseilServerInfo, TezosParameterFormat, KeyStore, StoreType, EntryPoint, OperationResult } from 'conseiljs';
+import * as StorageProcessor from './utilities/StorageProcessor'
 
 const tezosNode: string = 'https://tezos-dev.cryptonomic-infra.tech/';
-const keystore: KeyStore = {
+const keyStore: KeyStore = {
     publicKey: 'edpkuuGJ4ssH3N5k7ovwkBe16p8rVX1XLENiZ4FAayrcwUf9sCKXnG',
     privateKey: 'edskRpVqFG2FHo11aB9pzbnHBiPBWhNWdwtNyQSfEEhDf5jhFbAtNS41vg9as7LSYZv6rEbtJTwyyEg9cNDdcAkSr9Z7hfvquB',
     publicKeyHash: 'tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z',
@@ -11,76 +12,49 @@ const keystore: KeyStore = {
 const network: string = 'alphanet';
 const conseilServer: ConseilServerInfo = { url: 'https://conseil-dev.cryptonomic-infra.tech:443', apiKey: 'BUIDLonTezos-001' };
 
-<div class="entrypoint">
-    <span class="entrypointDescription">Transfer</span>
-    <input type="text" class="entrypointInput" id="transferValue" value="1000"></input>
-    <span class="entrypointDescription">from</span>
-    <input type="text" class="entrypointInput" id="transferFrom" value="tz1U8b7PV5w6WEyFNRtFX3RPGVTcmAUAgNn1"></input>
-    <span class="entrypointDescription">to</span>
-    <input type="text" class="entrypointInput" id="transferTo" value="tz1Po8u9NrkFKDsXgkiAYKfLrjvmao2TYBce"></input>                    
-
-    <button type="button" class="entrypointButton floatRight" onclick="transfer()">Transfer</button><br>
-</div>
-
 export async function viewEntryPoints() {
-    const entryPoints: EntryPoint[] = await generateEntryPoints();
+    const contractAddress: string = (<HTMLInputElement>document.getElementById('contractAddress')).value;
+    const entryPoints: EntryPoint[] = await getEntryPoints(contractAddress);
+
     let html = "";
     entryPoints.forEach(entryPoint => {
-        html += `<div class="entrypoint">`
+        html += `<div class="entrypoint">`;
         
-        html += `<span class="entrypointDescription">Transfer</span>`
-        html = html + (`${entryPoint.name}(${entryPoint.parameters.map(parameter => (parameter.name || 'unnamed') + '/' + parameter.type).join(', ')})`)
+        entryPoint.parameters.forEach((parameter, index) => {
+            html += `<span class="entrypointDescription">${parameter.name}/${parameter.type}</span>`;
+            html += `<input type="text" class="entrypointInput" id="${entryPoint.name + parameter.name + index}" value="${parameter.type}"></input>`;
+        })
 
-        html += `<button type="button" class="entrypointButton floatRight" onclick="transfer()">Transfer</button><br>`
-        html += `</div>`
-        html += `\n`
-    });
+        html += `<button type="button" class="entrypointButton floatRight" onclick="invokeEntryPoint('${entryPoint.name}', '${contractAddress}')">${entryPoint.name}</button><br>`;
+        html += `</div>`;
+    })
+
     document.getElementById('entryPoints').innerHTML = html;
 }
 
-export async function generateEntryPoints(): Promise<EntryPoint[]> {
-    const contractAddress: string = (<HTMLInputElement>document.getElementById('contractAddress')).value;
+export async function getEntryPoints(contractAddress: string): Promise<EntryPoint[]> {
     return await TezosContractIntrospector.generateEntryPointsFromAddress(conseilServer, network, contractAddress);
 }
 
-// Management Functions
+export async function invokeEntryPoint(entryPointName: string, contractAddress: string) {
+    const entryPoints: EntryPoint[] = await getEntryPoints(contractAddress);
+    for (let i = 0; i < entryPoints.length; i++) {
+        if (entryPoints[i].name == entryPointName) {
+            const entryPoint = entryPoints[i];
+            let args: string[] = [];
 
-/*
-export async function burn() {
-    const from: string = (<HTMLInputElement>document.getElementById('burnFrom')).value;
-    const value: number = parseInt((<HTMLInputElement>document.getElementById('burnValue')).value);
+            entryPoint.parameters.forEach((parameter, index) => {
+                args.push((<HTMLInputElement>document.getElementById(`${entryPoint.name + parameter.name + index}`)).value);
+            })
+            const invocationParameter: string = entryPoint.generateParameter(...args);
 
-    const result: operationResult = await TezosManagedLedger.burn(from, value, generateOpArgs());
-    renewStorage(result);
+            const result: OperationResult = await TezosNodeWriter.sendContractInvocationOperation(tezosNode, keyStore, contractAddress, 0, 50000, '', 1000, 100000, invocationParameter, TezosParameterFormat.Michelson);
+            renewStorage(result);
+        }
+    }
 }
 
-// View Functions
-
-export async function viewStorage() {
-    const storage = await TezosManagedLedger.viewStorage(getContractAddress(), conseilServer, network);
-
-    const ledger: string = storage.ledger;
-    const admin: string = storage.admin;
-    const paused: string = storage.paused;
-    const totalSupply: string = storage.totalSupply;
-
-    document.getElementById('contractStorage').innerHTML = `
-        Ledger: ${ledger} <br>
-        Admin: ${admin} <br>
-        Paused: ${paused} <br>
-        Total Supply: ${totalSupply}
-    `;
-}
-
-export async function viewAllowance() {
-    const address: string = '"' + (<HTMLInputElement>document.getElementById('viewAllowanceAddress')).value + '"';
-    const allowance = await TezosManagedLedger.viewAllowance(address, getContractAddress(), conseilServer, network);
-    document.getElementById('console').innerHTML = `Allowance from ${address} is ${allowance} <img src="./images/token.png" class="tokenIcon">`;
-}
-
-// Webpage Functions
-
-export async function renewStorage(result: operationResult) {
+export async function renewStorage(result: OperationResult) {
     let waitState = 1;
     const previousStorage = document.getElementById('contractStorage').innerHTML;
     while (document.getElementById('contractStorage').innerHTML === previousStorage) {
@@ -106,17 +80,11 @@ async function delay(milliseconds: number) {
     });
 }
 
+export async function viewStorage() {
+    const storage: string[] = await StorageProcessor.processStorage(getContractAddress(), conseilServer, network);
+    document.getElementById('contractStorage').innerHTML = `${storage}`;
+}
+
 export function getContractAddress(): string {
     return (<HTMLInputElement>document.getElementById('contractAddress')).value;
 }
-
-export function generateOpArgs(): operationArguments {
-    const contractAddress = getContractAddress();
-    return {
-        server: tezosNode,
-        keyStore: keystore,
-        to: contractAddress
-    };
-}
-
-*/
