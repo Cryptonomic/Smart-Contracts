0c2d83a69840207c064223811f359a162aaeec76
itamar <itamar.reif@gmail.com>
2020-01-14 21:27:03 -0500

    reorganized error checking and implemented registration period, need to fix checkExpired (and figure out timestamp comparisons for smartpy)

--- tezos/smartpy/tns/tns-domainmanager.py
+++ tezos/smartpy/tns/tns-domainmanager.py
@@ -4,72 +4,91 @@ import smartpy as sp
 class TNSDomainManager(sp.Contract):
     def __init__(self, owner, stamp):
         self.init(domainOwner = owner,
-            domainRegistry = sp.big_map(
+            nameRegistry = sp.big_map(
                 tkey = sp.TString,
                 tvalue = sp.TRecord(
                     name = sp.TString, 
                     owner = sp.TAddress,
                     resolver = sp.TAddress, 
-                    ttl = sp.TNat)),
-            stamp = stamp)
+                    registeredAt = sp.TTimestamp,
+                    registrationPeriod = sp.TNat)),
+            stamp = stamp,
+            addressRegistry = sp.big_map(
+                tkey = sp.TAddress,
+                tvalue = sp.TString))
 
-    # @param (subdomainName, resolver, ttl)
+    # @param (name, resolver, registrationPeriod)
     @sp.entry_point
-    def registerSubdomain(self, params):
-        self.isSubdomainValid(params.subdomainName)
-        self.isSubdomainAvailable(params.subdomainName)
-        self.data.domainRegistry[params.subdomainName] = sp.record(
-            name = params.subdomainName, 
-            owner = sp.sender, 
+    def registerName(self, params):
+        self.validateName(params.name)
+        self.validateAvailable(params.name)
+        self.data.nameRegistry[params.name] = sp.record(
+            name = params.name, 
+            owner = sp.sender,
             resolver = params.resolver,
-            ttl = params.ttl)
+            registeredAt = sp.now,
+            registrationPeriod = params.registrationPeriod)
+        self.data.addressRegistry[params.resolver] = params.name
 
-    # @param (subdomainName, newSubdomainOwner)
+    # @param (name, newNameOwner)
     @sp.entry_point
-    def transferSubdomainOwnership(self, params):
-        self.canUpdate(sp.sender, params.subdomainName)
-        self.data.domainRegistry[params.subdomainName].owner = params.newSubdomainOwner
+    def transferNameOwnership(self, params):
+        self.validateUpdate(sp.sender, params.name)
+        self.data.nameRegistry[params.name].owner = params.newNameOwner
 
-    # @param (subdomain, resolver)
+    # @param (name, resolver)
     @sp.entry_point
     def updateResolver(self, params):
-       self.canUpdate(sp.sender, params.subdomainName)
-       self.data.domainRegistry[params.subdomainName].resolver = params.resolver
+       self.validateUpdate(sp.sender, params.name)
+       self.data.nameRegistry[params.name].resolver = params.resolver
 
-    # @param (subdomain, ttl)
+    # @param (name, registrationPeriod)
     @sp.entry_point
-    def updateTTL(self, params):
-       self.canUpdate(sp.sender, params.subdomainName)
-       self.data.domainRegistry[params.subdomainName].ttl = params.ttl
+    def updateRegistrationPeriod(self, params):
+       self.validateUpdate(sp.sender, params.name)
+       self.data.nameRegistry[params.name].registrationPeriod = params.registrationPeriod
 
-    # @param subdomain
+    # @param name
     @sp.entry_point
-    def deleteSubdomain(self, params):
-       self.canUpdate(sp.sender, params.subdomainName)
-       del self.data.domainRegistry[params.subdomainName]
+    def deleteName(self, params):
+       self.validateUpdate(sp.sender, params.name)
+       del self.data.addressRegistry[self.data.nameRegistry[params.name].resolver]
+       del self.data.nameRegistry[params.name]
 
-    # Verify that subdomainName is valid
-    def isSubdomainValid(self, subdomainName):
-        sp.verify(subdomainName != "", "Invalid subdomain name: Empty")
+    # Verify that name is valid
+    def checkName(self, name):
+        return name != ""
 
-    # Verify that subdomainName is not already registered
-    def isSubdomainAvailable(self, subdomainName):
-        sp.verify(~(self.data.domainRegistry.contains(subdomainName)),
-            "Subdomain is not available")
+    # Verify that the name is registered
+    def checkRegistered(self, name):
+        return self.data.nameRegistry.contains(name)
+
+    # Verify that name has not expired, requires name to be in self.data.nameRegistry
+    def checkExpired(self, name):
+        return sp.now <= self.data.nameRegistry[name].registeredAt.add_seconds(self.data.nameRegistry[name].registrationPeriod)
 
     # Verify that the invoker has update rights on the record requested
-    def validPermissions(self, sender, record):
-        sp.verify(sender == record.owner,
-            "Sender does not have permissions to change this subdomain")
+    def checkNamePermissions(self, sender, record):
+        return sender == record.owner
 
+    # Verify that name is valid
+    def validateName(self, name):
+        sp.verify(self.checkName(name), "Invalid name")
+
+    # Verify that name is not already registered
+    def validateAvailable(self, name):
+        sp.verify(~(self.checkRegistered(name) & ~(self.checkExpired(name))),
+            "Name is currently registered")
+        #sp.if (self.checkRegistered(name) & ~(self.checkExpired(name))):
+        #    sp.failwith(
+        
     # Performs all checks to see if the transaction can update the record 
-    # corresponding to subdomainName.
-    # @param subdomainName name of subdomain to be updated
-    def canUpdate(self, sender, subdomainName):
-        self.isSubdomainValid(subdomainName)
-        sp.verify(self.data.domainRegistry.contains(subdomainName),
-            "Subdomain is not registered")
-        self.validPermissions(sender, self.data.domainRegistry[subdomainName])
+    # corresponding to name.
+    def validateUpdate(self, sender, name):
+        sp.verify(self.checkRegistered(name), "Name not registered")
+        sp.verify(self.checkExpired(name), "Name registration has expired")
+        sp.verify(self.checkNamePermissions(sender, self.data.nameRegistry[name]),
+            "Invalid permissions")
 
 @sp.add_test("TNSDomainManagerTest")
 def test():
@@ -81,8 +100,8 @@ def test():
     ownerAddr = sp.address("tz1-owner")
     resolverAddr = sp.address("tz1-resolver")
     testAddr = sp.address("tz1-notOwnerOrManager")
-    subdomainName1 = "domain1"
-    testTtl = 1
+    name1 = "domain1"
+    regPeriod = 600
 
     # init contract
     domainManager = TNSDomainManager(ownerAddr, "test-stamp")
@@ -91,67 +110,77 @@ def test():
     # test entry points
     scenario.h2("Testing registerDomain entrypoint")
     scenario.h3("Testing successful domain registration")
-    scenario += domainManager.registerSubdomain(
-        subdomainName = subdomainName1,
+    scenario += domainManager.registerName(
+        name = name1,
         resolver = resolverAddr,
-        ttl = testTtl).run(sender = ownerAddr)
-    scenario.verify(domainManager.data.domainRegistry[subdomainName1] ==
-        sp.record(name = subdomainName1,
+        registrationPeriod = regPeriod).run(
+            sender = ownerAddr, 
+            now = sp.timestamp(0))
+    scenario.verify(domainManager.data.nameRegistry[name1] ==
+        sp.record(name = name1,
             owner = ownerAddr,
             resolver = resolverAddr,
-            ttl = testTtl)) 
+            registeredAt = sp.timestamp(0),
+            registrationPeriod = regPeriod))
+    scenario.verify(domainManager.data.addressRegistry[resolverAddr] == name1)
     scenario.h3("Testing failed domain registration - already exists")
-    scenario += domainManager.registerSubdomain(
-        subdomainName = subdomainName1,
+    scenario += domainManager.registerName(
+        name = name1,
         resolver = resolverAddr,
-        ttl = testTtl).run(sender = ownerAddr, valid = False)
+        registrationPeriod = regPeriod).run(
+            sender = ownerAddr,
+            now = sp.timestamp(30),
+            valid = False)
     # verify storage is unchanged
-    scenario.verify(domainManager.data.domainRegistry[subdomainName1] ==
-        sp.record(name = subdomainName1,
+    scenario.verify(domainManager.data.nameRegistry[name1] ==
+        sp.record(name = name1,
             owner = ownerAddr,
             resolver = resolverAddr,
-            ttl = testTtl))
+            registeredAt = sp.timestamp(0),
+            registrationPeriod = regPeriod))
     scenario.h3("Testing invalid name registration")
-    scenario += domainManager.registerSubdomain(
-            subdomainName = "",
+    scenario += domainManager.registerName(
+            name = "",
             resolver = resolverAddr,
-            ttl = testTtl).run(sender = ownerAddr, valid = False)
-    scenario.verify(~(domainManager.data.domainRegistry.contains("")))
+            registrationPeriod = regPeriod).run(
+                sender = ownerAddr, 
+                now = sp.timestamp(1),
+                valid = False)
+    scenario.verify(~(domainManager.data.nameRegistry.contains("")))
 
     scenario.h2("Testing domain updates")
     scenario.h3("Testing successful resovler update")
     newResolverAddr = sp.address("tz1-newResolver")
     scenario += domainManager.updateResolver(
-            subdomainName = subdomainName1,
+            name = name1,
             resolver = newResolverAddr).run(sender = ownerAddr)
-    scenario.verify(domainManager.data.domainRegistry[subdomainName1].resolver == newResolverAddr)
+    scenario.verify(domainManager.data.nameRegistry[name1].resolver == newResolverAddr)
 
     scenario.h3("Testing successful TTL update")
     newTTL = 2
     scenario += domainManager.updateTTL(
-            subdomainName = subdomainName1,
-            ttl = newTTL).run(sender = ownerAddr)
-    scenario.verify(domainManager.data.domainRegistry[subdomainName1].ttl == newTTL)
+            name = name1,
+            registrationPeriod = newTTL).run(sender = ownerAddr)
+    scenario.verify(domainManager.data.nameRegistry[name1].registrationPeriod == newTTL)
 
     scenario.h3("Testing successful ownership transfer")
     newOwnerAddr = sp.address("tz1-newOwner")
-    scenario += domainManager.transferSubdomainOwnership(
-            subdomainName = subdomainName1,
-            newSubdomainOwner = newOwnerAddr).run(sender = ownerAddr)
-    scenario.verify(domainManager.data.domainRegistry[subdomainName1].owner == newOwnerAddr)
+    scenario += domainManager.transferNameOwnership(
+            name = name1,
+            newNameOwner = newOwnerAddr).run(sender = ownerAddr)
+    scenario.verify(domainManager.data.nameRegistry[name1].owner == newOwnerAddr)
     scenario.h3("Testing that old owner cannot modify subdomain")
-    scenario += domainManager.transferSubdomainOwnership(
-            subdomainName = subdomainName1,
-            newSubdomainOwner = ownerAddr).run(sender = ownerAddr, valid = False)
-    scenario.verify(domainManager.data.domainRegistry[subdomainName1].owner == newOwnerAddr)
+    scenario += domainManager.transferNameOwnership(
+            name = name1,
+            newNameOwner = ownerAddr).run(sender = ownerAddr, valid = False)
+    scenario.verify(domainManager.data.nameRegistry[name1].owner == newOwnerAddr)
 
     scenario.h2("Testing subdomain deletion")
     scenario.h3("Testing unsuccessful deletion of unregistered/deleted domain")
-    unregisteredSubdomainName = "domain2"
-    scenario += domainManager.deleteSubdomain(subdomainName = unregisteredSubdomainName).run(sender = newOwnerAddr, valid = False)
+    unregisteredName = "domain2"
+    scenario += domainManager.deleteName(name = unregisteredName).run(sender = newOwnerAddr, valid = False)
     scenario.h3("Testing unsuccessful deletion of domain because of bad permissions")
-    scenario += domainManager.deleteSubdomain(subdomainName = subdomainName1).run(sender = ownerAddr, valid = False)
+    scenario += domainManager.deleteName(name = name1).run(sender = ownerAddr, valid = False)
     scenario.h3("Testing successful deletion")
-    scenario += domainManager.deleteSubdomain(subdomainName = subdomainName1).run(sender = newOwnerAddr)
-    scenario.verify(~(domainManager.data.domainRegistry.contains(subdomainName1)))
-
+    scenario += domainManager.deleteName(name = name1).run(sender = newOwnerAddr)
+    scenario.verify(~(domainManager.data.nameRegistry.contains(name1)))
