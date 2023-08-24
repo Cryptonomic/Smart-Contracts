a22a628cb8b27a41feffb9551ef45caaa25a35c1
dwariyar <dwariyar310@gmail.com>
2023-08-23 22:45:38 -0400

    current remix unit tests

--- Ethereum/Voting/_Quadratic_Voting_test.sol
+++ Ethereum/Voting/_Quadratic_Voting_test.sol
@@ -14,22 +14,17 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 // File name has to end with '_test.sol', this file can contain more than one testSuite contracts
 contract testSuite {
     
-    Ballot testBallot = new Ballot(testProposalNames);
-
     bytes32[] testProposalNames;
+    Ballot testBallot = new Ballot(testProposalNames);
     address public acc0;  
     address public acc1;
     address public acc2;
     address public acc3;
 
-    /*try new Ballot(testProposalNames) returns (Ballot testBallot){
-        emit Log ("ballot successfully created");
-    } catch Error*/
-
     function beforeAll() public {
-        testProposalNames.push(0x6669727374000000000000000000000000000000000000000000000000000000);
-        testProposalNames.push(0x7365636f6e640000000000000000000000000000000000000000000000000000);
-        testProposalNames.push(0x7468697264000000000000000000000000000000000000000000000000000000);
+        testProposalNames.push("first");
+        testProposalNames.push("second");
+        testProposalNames.push("third");
         acc0 = TestsAccounts.getAccount(0);
         acc1 = TestsAccounts.getAccount(1);
         acc2 = TestsAccounts.getAccount(2);
@@ -37,59 +32,64 @@ contract testSuite {
     }
     
     function testMessageSenderIsChairperson() public returns (bool) {
-        Assert.equal(testBallot.chairperson(), acc0, "Message sender should be chairperson");
+        return Assert.equal(acc0, testBallot.chairperson(), "Message sender should be chairperson");
     }
     
     function testChairpersonGets100Credits() public returns (bool) {
-        Assert.equal(testBallot.getVoterCreditBank(acc0), 100, "Chairperson should have 100 vote credits");
+        return Assert.equal(testBallot.getVoterCreditBank(testBallot.chairperson()), 100, "Chairperson should have 100 vote credits");
     }
 
 
-    function testProposalsStartWith0Votes() public returns (bool) {
+    function testProposalsStartWith0Votes() public {
         for (uint i = 0; i < testProposalNames.length; i++) {
             Assert.equal(testBallot.getVoteCount(testProposalNames[i]), 0, "Proposal does not start with zero votes");
         }    
     }  
 
     function testOnlyChairpersonGrantsRightToVote() public returns (bool) {
-        testBallot.giveRightToVote(acc2,acc1);
-        return Assert.equal(testBallot.getVoterCreditBank(acc2), 0, "Only chairperson can assign right to vote");
+        testBallot.giveRightToVote(acc1);
+        return Assert.equal(testBallot.getVoterCreditBank(acc1), 0, "Only chairperson can assign right to vote");
     }
     
-    function testRightToVoteGivenToEligibleVoter() public returns (bool) {
-        testBallot.giveRightToVote(acc1, acc0);
-        if (testBallot.getVoterCreditSpent(acc1) == 0) {
-            return Assert.equal(testBallot.getVoterCreditBank(acc1), 100, "Voter should be given voter credits");
+    function testRightToVoteGivenToEligibleVoter() public  {
+        testBallot.giveRightToVote(acc2);
+        if (testBallot.getVoterCreditSpent(acc2) == 0) {
+            Assert.equal(testBallot.getVoterCreditBank(acc2), 100, "Voter should be given voter credits");
         }
     }
 
     function testRightToVoteNotGivenToIneligibleVoter() public returns (bool) {
-        testBallot.useVoterCredit(0, acc2);
-        testBallot.giveRightToVote(acc2, acc0);
-        return Assert.equal(testBallot.getVoterCreditBank(acc2), 0, "Ineligible voter given credits");
+        bool result;
+        testBallot.useVoterCredit(0);
+        try testBallot.giveRightToVote(acc3){
+            result = Assert.equal(testBallot.getVoterCreditBank(acc3), 99, "Ineligible voter given credits");
+        }
+        catch (bytes memory) {
+            result = false;
+        }
+        return result;
     }
  
     function testValuesAdjustedWhenVoterCreditSpent() public returns (bool) {
-    testBallot.giveRightToVote(acc3, acc0);
+    testBallot.giveRightToVote(acc3);
 
-            testBallot.useVoterCredit(0, acc3);
+            testBallot.useVoterCredit(0);
             Assert.equal(testBallot.getVoterCreditSpent(acc3), 1, "Voter credits not adjusted correctly");
             Assert.equal(testBallot.getVoterCreditBank(acc3), 99, "Voter credits not adjusted correctly");
 
-            testBallot.useVoterCredit(1, acc3);
+            testBallot.useVoterCredit(1);
             Assert.equal(testBallot.getVoterCreditSpent(acc3), 2, "Voter credits not adjusted correctly");
             Assert.equal(testBallot.getVoterCreditBank(acc3), 98, "Voter credits not adjusted correctly");
 
         return true;
     }        
     
-    function testVoteCountIsSquareRootOfVoterCredits() public returns (bool) {
-        testBallot.giveRightToVote(acc1, acc0);
-        testBallot.useVoterCredit(0, acc1);
-        testBallot.useVoterCredit(0, acc1);
-        testBallot.useVoterCredit(0, acc1);
-        testBallot.useVoterCredit(0, acc1);
-        testBallot.useVoterCredit(1, acc1);
+    function testVoteCountIsSquareRootOfVoterCredits() public {
+        testBallot.useVoterCredit(0);
+        testBallot.useVoterCredit(0);
+        testBallot.useVoterCredit(0);
+        testBallot.useVoterCredit(0);
+        testBallot.useVoterCredit(1);
 
         testBallot.calcVoteCount();
 
@@ -99,11 +99,8 @@ contract testSuite {
     }   
     
     function testCorrectWinningProposalChosen() public returns (bool) {
-        testBallot.giveRightToVote(acc1, acc0);
-        testBallot.useVoterCredit(0, acc1);
-        testBallot.useVoterCredit(0, acc1);
-        testBallot.useVoterCredit(1, acc1);
-
+        testBallot.useVoterCredit(0);
+        testBallot.calcVoteCount();
         uint winningProposal = testBallot.chooseWinningProposal();
         return Assert.equal(winningProposal, 0, "Incorrect winning proposal selected");
     }
