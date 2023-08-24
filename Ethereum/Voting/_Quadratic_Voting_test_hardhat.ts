55614d0158afe851d21a5c3f5948df192e7a0656
dwariyar <dwariyar310@gmail.com>
2023-08-23 22:34:18 -0400

    changes for review

--- Ethereum/Voting/_Quadratic_Voting_test_hardhat.ts
+++ Ethereum/Voting/_Quadratic_Voting_test_hardhat.ts
@@ -1,96 +1,111 @@
-const { ethers } = require("hardhat");
-import { expect } from 'chai';
-import { Signer } from 'ethers';
+const { ethers } = require ('hardhat');
+const { expect } = require ('chai');
+const { Signer } = require ('ethers');
 
-describe('Ballot Test', () => {
-  let testBallot: any; // Replace 'any' with the contract type
 
-  let acc0: Signer;
-  let acc1: Signer;
-  let acc2: Signer;
-  let acc3: Signer;
+describe('Ballot Contract', () => {
+let testBallot: any; // Replace 'any' with the contract type
 
-  beforeEach(async () => {
-    const Ballot = await ethers.getContractFactory('Ballot');
-    testBallot = await Ballot.deploy(['first', 'second', 'third']);
-    await testBallot.deployed();
 
-    const accounts = await ethers.getSigners();
-    acc0 = accounts[0];
-    acc1 = accounts[1];
-    acc2 = accounts[2];
-    acc3 = accounts[3];
-  });
+let acc0: typeof Signer;
+let acc1: typeof Signer;
+let acc2: typeof Signer;
+let acc3: typeof Signer;
 
-  it('should have the correct chairperson', async () => {
-    expect(await testBallot.chairperson()).to.equal(await acc0.getAddress());
-  });
 
-  it('should give 100 credits to the chairperson', async () => {
-    expect(await testBallot.getVoterCreditBank(await acc0.getAddress())).to.equal(100);
-  });
+beforeEach(async () => {
+const Ballot = await ethers.getContractFactory('Ballot');
+testBallot = await Ballot.deploy(['first', 'second', 'third']);
+await testBallot.deployed();
+const [accounts] = await ethers.getSigners();
+acc0 = accounts[0];
+acc1 = accounts[1];
+acc2 = accounts[2];
+acc3 = accounts[3];
+});
 
-  it('should start proposals with 0 votes', async () => {
-    for (let i = 0; i < 3; i++) {
-      expect(await testBallot.getVoteCount(await testBallot.testProposalNames(i))).to.equal(0);
-    }
-  });
 
-  it('should only allow chairperson to grant right to vote', async () => {
-    await testBallot.giveRightToVote(await acc1.getAddress());
-    expect(await testBallot.getVoterCreditBank(await acc1.getAddress())).to.equal(0);
-  });
+it('should have the correct chairperson', async () => {
+expect(await testBallot.chairperson).to.equal(acc0);
+});
 
-  it('should give right to vote to eligible voters', async () => {
-    await testBallot.giveRightToVote(await acc2.getAddress());
-    expect(await testBallot.getVoterCreditBank(await acc2.getAddress())).to.equal(100);
-  });
 
-  it('should not give right to vote to ineligible voters', async () => {
-    await testBallot.useVoterCredit(0);
-    let result = true;
-    try {
-      await testBallot.giveRightToVote(await acc3.getAddress());
-    } catch {
-      result = false;
-    }
-    expect(await testBallot.getVoterCreditBank(await acc3.getAddress())).to.equal(0);
-    expect(result).to.be.false;
-  });
+it('should give 100 credits to the chairperson', async () => {
+expect(await testBallot.getVoterCreditBank(testBallot.chairperson).to.equal(100));
+});
 
-  it('should adjust values when voter credits are spent', async () => {
-    await testBallot.giveRightToVote(await acc3.getAddress());
 
-    await testBallot.useVoterCredit(0);
-    expect(await testBallot.getVoterCreditSpent(await acc3.getAddress())).to.equal(1);
-    expect(await testBallot.getVoterCreditBank(await acc3.getAddress())).to.equal(99);
+it('should start proposals with 0 votes', async () => {
+for (let i = 0; i < 3; i++) {
+expect(await testBallot.getVoteCount(await testBallot.testProposalNames(i))).to.equal(0);
+}
+});
 
-    await testBallot.useVoterCredit(1);
-    expect(await testBallot.getVoterCreditSpent(await acc3.getAddress())).to.equal(2);
-    expect(await testBallot.getVoterCreditBank(await acc3.getAddress())).to.equal(98);
-  });
 
-  it('should calculate vote count as square root of voter credits', async () => {
-    await testBallot.useVoterCredit(0);
-    await testBallot.useVoterCredit(0);
-    await testBallot.useVoterCredit(0);
-    await testBallot.useVoterCredit(0);
-    await testBallot.useVoterCredit(1);
+it('should only allow chairperson to grant right to vote', async () => {
+await testBallot.giveRightToVote(acc1);
+expect(await testBallot.getVoterCreditBank(acc1).to.equal(0));
+});
 
-    await testBallot.calcVoteCount();
 
-    expect(await testBallot.getVoteCount(await testBallot.testProposalNames(0))).to.equal(2);
-    expect(await testBallot.getVoteCount(await testBallot.testProposalNames(1))).to.equal(1);
-    expect(await testBallot.getVoteCount(await testBallot.testProposalNames(2))).to.equal(0);
-  });
+it('should give right to vote to eligible voters', async () => {
+await testBallot.giveRightToVote(acc2);
+expect(await testBallot.getVoterCreditBank(acc2).to.equal(100));
+});
 
-  it('should choose the correct winning proposal', async () => {
-    await testBallot.useVoterCredit(0);
-    await testBallot.calcVoteCount();
-    const winningProposal = await testBallot.chooseWinningProposal();
 
-    expect(winningProposal).to.equal(0);
-  });
+it('should not give right to vote to ineligible voters', async () => {
+await testBallot.useVoterCredit(0);
+let result = true;
+try {
+await testBallot.giveRightToVote(acc3);
+} catch {
+result = false;
+}
+expect(await testBallot.getVoterCreditBank(acc3)).to.equal(0);
+expect(result).to.be.false;
+});
 
+
+it('should adjust values when voter credits are spent', async () => {
+await testBallot.giveRightToVote(acc3);
+
+
+await testBallot.useVoterCredit(0);
+expect(await testBallot.getVoterCreditSpent(acc3)).to.equal(1);
+expect(await testBallot.getVoterCreditBank(acc3)).to.equal(99);
+
+
+await testBallot.useVoterCredit(1);
+expect(await testBallot.getVoterCreditSpent(acc3)).to.equal(2);
+expect(await testBallot.getVoterCreditBank(acc3)).to.equal(98);
+});
+
+
+it('should calculate vote count as square root of voter credits', async () => {
+await testBallot.useVoterCredit(0);
+await testBallot.useVoterCredit(0);
+await testBallot.useVoterCredit(0);
+await testBallot.useVoterCredit(0);
+await testBallot.useVoterCredit(1);
+
+
+await testBallot.calcVoteCount();
+
+
+expect(await testBallot.getVoteCount(await testBallot.testProposalNames(0))).to.equal(2);
+expect(await testBallot.getVoteCount(await testBallot.testProposalNames(1))).to.equal(1);
+expect(await testBallot.getVoteCount(await testBallot.testProposalNames(2))).to.equal(0);
+});
+
+
+it('should choose the correct winning proposal', async () => {
+await testBallot.useVoterCredit(0);
+await testBallot.calcVoteCount();
+const winningProposal = await testBallot.chooseWinningProposal();
+
+
+expect(winningProposal).to.equal(0);
+});
 
 });
